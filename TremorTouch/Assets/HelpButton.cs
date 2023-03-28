f0ccc8441e7724c39a59bb1b8d2f08ee8d54fd4f
sobeskes <sobeskes@umich.edu>
2023-03-28 15:52:05 -0400

    added help button that brings up the welcome message

---
+++ TremorTouch/Assets/HelpButton.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HelpButton : MonoBehaviour
+{
+    public Button button;
+    public GameObject manager;
+    public GameObject helpPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(OnClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnClick()
+    {
+        manager.GetComponent<Manager>().firstUse = true;
+        helpPanel.SetActive(true);
+    }
+}
